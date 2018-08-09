358a19c5048ea102777a1c6320da60e3e9686257
Alianny Melina Perez <ampmelina222@gmail.com>
2018-08-09 22:52:23 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+function shout(string) {
+   return string.toUpperCase()
+}
+function whisper(string) {
+   return string.toLowerCase()
+}
+function logShout(string) {
+   console.log(shout(string))    
+}
+function logWhisper(string) {
+   console.log(whisper(string))
+}
+function sayHiToGrandma(string) {
+  if (string.toLowerCase()) 
+   return (("I can't hear you!"))
+} 
+function sayHiToGrandma(string) {
+  if (string.toUpperCase()
+  return (("YES INDEED!"))
+}
+function sayHiToGrandma()
+  if ("I love you, Grandma.")
