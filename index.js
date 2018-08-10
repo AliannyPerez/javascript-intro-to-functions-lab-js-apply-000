f50a805b65206143b9d8210443d2eae35fecd8b5
Alianny Melina Perez <ampmelina222@gmail.com>
2018-08-10 00:03:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ function sayHiToGrandma(string) {
   else if (string === string.toUpperCase()) 
   return (("YES INDEED!"))
 }
-  if (string === "I love you, Grandma."
-   return (("I love you, too"))
+  else (string("I love you, Grandma.")) {
+   return ("I love you, too")
 }
 // function sayHiToGrandma(){
 //   if (("I love you, Grandma."))
